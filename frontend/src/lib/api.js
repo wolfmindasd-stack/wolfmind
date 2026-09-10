d1699e165e2f476a040eaed4757d6153e6f46dcd
wolfmindasd-stack <wolfmind.asd@gmail.com>
2026-09-10 23:47:38 +0200

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,3 @@
-// frontend/src/lib/api.js
 import axios from "axios";
 import { API_URL } from "../config";
 
@@ -6,17 +5,3 @@ export const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
 });
-
-// Formattazioni di utilità (mantieni quelle che avevi)
-export function fmtEur(value) {
-  return new Intl.NumberFormat("it-IT", {
-    style: "currency",
-    currency: "EUR",
-    maximumFractionDigits: 2,
-  }).format(value ?? 0);
-}
-
-export function fmtDate(value) {
-  if (!value) return "";
-  return new Intl.DateTimeFormat("it-IT").format(new Date(value));
-}
